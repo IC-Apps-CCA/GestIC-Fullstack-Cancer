dfa87ab8d6986274a38e5e315e35112e79f98cfa
Jhonnye Farias <jgof@ic.ufal.br>
2022-07-07 22:03:13 -0300

    fix(seed): fix field names

--- backend/src/database/seeders/20210901205431-informative.js
+++ backend/src/database/seeders/20210901205431-informative.js
@@ -5,16 +5,16 @@ module.exports = {
     const uuid = require('uuid');
     return await queryInterface.bulkInsert('informative', [
       {
-        id: uuid.v4(),
-        owner: 'f9584d5c-b11e-4148-8e12-e124782f9b9c',
-        title: 'Aplicação Gestic recebe deploy',
-        content: 'O desafio de tornar a aplicação acessível e integrada com o backend foi um sucesso!.',
+        info_id: uuid.v4(),
+        owner_id: 'f9584d5c-b11e-4148-8e12-e124782f9b9c',
+        info_title: 'Aplicação Gestic recebe deploy',
+        info_content: 'O desafio de tornar a aplicação acessível e integrada com o backend foi um sucesso!.',
       },
       {
-        id: uuid.v4(),
-        owner: 'f9584d5c-b11e-4148-8e12-e124782f9b9c',
-        title: 'Ampliação no calendário acadêmico',
-        content: 'Devido à fortes chuvas no período de verão, o calendário acadêmico foi ampliado em uma semana, permitindo cumprir os prazos curriculares.',
+        info_id: uuid.v4(),
+        owner_id: 'f9584d5c-b11e-4148-8e12-e124782f9b9c',
+        info_title: 'Ampliação no calendário acadêmico',
+        info_content: 'Devido à fortes chuvas no período de verão, o calendário acadêmico foi ampliado em uma semana, permitindo cumprir os prazos curriculares.',
       },
     ]);
   },
