bf35fcabbc7c6bfb8b785723f2f11802bdaf71d3
Jhonnye Farias <jgof@ic.ufal.br>
2022-07-07 22:08:05 -0300

    fix(seed): fix relation names

--- backend/src/database/seeders/20210901205431-informative.js
+++ backend/src/database/seeders/20210901205431-informative.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const uuid = require('uuid');
-    return await queryInterface.bulkInsert('informative', [
+    return await queryInterface.bulkInsert('informatives', [
       {
         info_id: uuid.v4(),
         owner_id: 'f9584d5c-b11e-4148-8e12-e124782f9b9c',
@@ -20,6 +20,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    return await queryInterface.bulkDelete('informative', null, {});
+    return await queryInterface.bulkDelete('informatives', null, {});
   }
 };
